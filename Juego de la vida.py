841ad6e74f3aba84dbfd24d1afd7791251f47688
Vodka1200 <54092247+Vodka1200@users.noreply.github.com>
2019-09-22 00:54:33 -0500

    Delete Juego de la vida.py

--- Juego de la vida.py
+++
@@ -1,9 +0,0 @@
-from Array2D importArray2D
-class juegoDeLAvIDA:
-    def __init__(self,rows,cols,generaciones,poblacion_inicial):
-        self.cuadro=Array2D(rows,cols)
-        self.__=rows
-        self.__columnas=colsself.__generaciones=generaciones
-        self.__cuadro.clearing(0)
-        for cell in poblacion_inicial:
-            self.__cuadro.set_item(cell[0],cell[1],1)
